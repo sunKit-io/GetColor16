7746e96885e58dc8291608810561c99db298cfa6
sunKit-io <873235315@qq.com>
2016-09-09 16:19:11 +0800

    UIColor获取16进制颜色
    UIColor获取16进制颜色

---
+++ GetColor16.h
@@ -0,0 +1,14 @@
+//
+//  FYTextField+Create.h
+//  FeiYing
+//
+//  Created by FeiYing on 15/7/22.
+//  Copyright (c) 2015年 FeiYing. All rights reserved.
+//
+#import <Foundation/Foundation.h>
+
+@interface GetColor16 : NSObject
+
++(UIColor *)hexStringToColor: (NSString *) stringToConvert;
+
+@end
---
+++ GetColor16.m
@@ -0,0 +1,44 @@
+//
+//  FYTextField+Create.h
+//  FeiYing
+//
+//  Created by FeiYing on 15/7/22.
+//  Copyright (c) 2015年 FeiYing. All rights reserved.
+//
+
+#import "GetColor16.h"
+
+@implementation GetColor16
+
++(UIColor *)hexStringToColor: (NSString *) stringToConvert
+{
+    NSString *cString = [[stringToConvert stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] uppercaseString];
+    // String should be 6 or 8 charactersif ([cString length] < 6) return [UIColor blackColor];
+    // strip 0X if it appearsif ([cString hasPrefix:@"0X"]) cString = [cString substringFromIndex:2];
+    if ([cString hasPrefix:@"#"]) cString = [cString substringFromIndex:1];
+    if ([cString length] != 6) return [UIColor blackColor];
+    
+    // Separate into r, g, b substrings
+    NSRange range;
+    range.location = 0;
+    range.length = 2;
+    NSString *rString = [cString substringWithRange:range];
+    range.location = 2;
+    NSString *gString = [cString substringWithRange:range];
+    range.location = 4;
+    NSString *bString = [cString substringWithRange:range];
+    // Scan values
+    unsigned int r, g, b;
+    
+    [[NSScanner scannerWithString:rString] scanHexInt:&r];
+    [[NSScanner scannerWithString:gString] scanHexInt:&g];
+    [[NSScanner scannerWithString:bString] scanHexInt:&b];
+    
+    return [UIColor colorWithRed:((float) r / 255.0f)
+                           green:((float) g / 255.0f)
+                            blue:((float) b / 255.0f)
+                           alpha:1.0f];
+}
+
+@end
+
